6b202a6875ef553b7a23183343598a25bb4c5c1f
todoom1 <aa13976383@outlook.com>
2026-02-06 02:47:23 +0800

    Add files via upload

---
+++ list.js
@@ -0,0 +1,27 @@
+document.getElementById("st").addEventListener("click", function() {
+	window.open(atob("aHR0cHM6Ly9sZWp2YS50cHJzbWkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDU4JnRvcElkPTI3NDE1JnNlbGZQbGFuSWQ9MTA3NTY"), '_blank');
+	});
+document.getElementById("ff").addEventListener("click", function() {
+	window.open(atob("aHR0cHM6Ly9sbmNxYy5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDUwJnRvcElkPTc1NTE3NjM0ODMyNjY5MjYmc2VsZlBsYW5JZD0xNzQ1MjU="), '_blank');
+	});
+document.getElementById("yh").addEventListener("click", function() {
+	window.open(atob("aHR0cHM6Ly9nZnZneC5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD04MDAmcHQ9MDFENzhDREYtRTk0Mi00N0JDLUFFOTUtRjEyMzQyNTQzQkZD"), '_blank');
+	});
+document.getElementById("zt").addEventListener("click", function() {
+	window.open(atob("aHR0cHM6Ly9wa2FqaW0uaGZqY2wuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDMxJnB0PUYzREEzOUQyLTc5M0UtOTU1Qy1BMUM0LTNGMjUxNDY4RUJBNQ"), '_blank');
+	});
+document.getElementById("jl").addEventListener("click", function() {
+	window.open(atob("aHR0cHM6Ly90ZmFqbi5oY2p3ai5jb20vYXBwL3JlZ2lzdGVyLnBocD9zaXRlX2lkPTIwODEmdG9wSWQ9MjQwMDMmc2VsZlBsYW5JZD04MzQ"), '_blank');
+	});
+document.getElementById("sj").addEventListener("click", function() {
+	window.open(atob("aHR0cHM6Ly94anZ1ZWIuaGVmYXNoaXAuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDEwJnRvcElkPTMxMTMyMzUN"), '_blank');
+	});
+
+document.getElementById("stn").innerHTML = ("胜天国际");
+document.getElementById("ffn").innerHTML = ("非凡娱乐");
+document.getElementById("yhn").innerHTML = ("壹号娱乐");
+document.getElementById("ztn").innerHTML = ("征途国际");
+document.getElementById("jln").innerHTML = ("君临国际");
+document.getElementById("sjn").innerHTML = ("赏金国际");
+document.getElementById("2").innerHTML = ("十年信誉平台推荐");
+document.getElementById("1").innerHTML = ("XXP6.MY");
